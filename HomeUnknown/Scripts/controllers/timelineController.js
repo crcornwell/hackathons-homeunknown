711b36984429800ced35f19ec6cef0558871a6bf
cornix <christopherrcornwell@gmail.com>
2014-05-17 15:19:53 -0700

    angular works!

--- HomeUnknown/Scripts/controllers/timelineController.js
+++ HomeUnknown/Scripts/controllers/timelineController.js
@@ -1,6 +1,6 @@
 ﻿function timelineController($scope, $http) {
     $http({ method: 'GET', url: '/api/timelines/0' }).
     success(function (data) {
-        $scope.timelines = JSON.parse(data);
+        $scope.timelines = data;
     });
 }
