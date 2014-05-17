5f9d77179a5890d337fda2cf4f74d329351f0d81
cornix <christopherrcornwell@gmail.com>
2014-05-17 12:55:58 -0700

    API controller and ContentTypeEnum

---
+++ HomeUnknown/Enums/ContentType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeUnknown.Enums
+{
+    public enum ContentType
+    {
+        Text,
+        Photo,
+        Video
+    }
+}
