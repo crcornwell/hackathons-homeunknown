78c4101d9ce6261abbd9846485bf9ee172d21062
unionthugface <cowofevil@aol.com>
2014-05-18 03:14:29 -0700

    added morphbutton script bundle; created test controller; added test input forms

--- HomeUnknown/Controllers/TestController.cs
+++ HomeUnknown/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeUnknown.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,9 +15,44 @@ namespace HomeUnknown.Controllers
             return View();
         }
 
-        public ActionResult InputContent()
+        public ActionResult InputContent(Guid id)
         {
-            return View();
+            ContentModel model = new ContentModel();
+
+            model.EventId = id;
+
+            return View(model);
+        }
+
+        public ActionResult EventDisplay()
+        {
+            List<EventModel> eventList = null;
+            
+            HomeUnknownEntities entityHelper = new HomeUnknownEntities();
+
+            var eventResults = entityHelper.sp_sel_TimelineEvents_SelectByTimeline(Guid.Parse("06AE6ABB-0296-4F10-B6AF-3B13FE9B4FCF"));
+
+            if(eventResults != null)
+            {
+                foreach(var item in eventResults)
+                {
+                    if(eventList == null)
+                    {
+                        eventList = new List<EventModel>();
+                    }
+
+                    EventModel singleEvent = new EventModel();
+
+                    singleEvent.Id = item.Event_PK;
+                    singleEvent.Name = item.EventName;
+                    singleEvent.Location = item.EventLocation;
+                    singleEvent.Year = item.EventYear;
+                    singleEvent.TimelineId = Guid.Parse("06AE6ABB-0296-4F10-B6AF-3B13FE9B4FCF");
+
+                    eventList.Add(singleEvent);
+                }
+            }
+            return View(eventList.OrderBy(x => x.Year).ToList());
         }
     }
 }
